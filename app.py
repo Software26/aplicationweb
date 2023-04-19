6dda432b66aa17b1b98251f3846925325d35ce43
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-04-19 08:01:14 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD']='password'
+app.config['MYSQL_DB'] = 'flaskcontacts'
+mysql = MySQL(app)
+
+@app.route("/")
+def index():
+    return 'hello Word'
+
+@app.route('/add_contact')
+def contact():
+    return 'add contact'
+
+@app.route('/edit')
+def edit():
+    return "edit contact"
+
+@app.route("/delete")
+def delete():
+    return delete
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
